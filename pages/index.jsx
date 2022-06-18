513ed88891373c06abc93ba1b22970b7cf824536
marsen <marsenzakirov11@gmail.com>
2022-06-18 17:49:08 +0300

    Add own structure

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/header/header";
+import SideBar from "../components/sidebar/sidebar";
+const Index = () => {
+  return (
+    <>
+      <Header />
+      <SideBar />
+    </>
+  );
+};
+export default Index;
