6c05c0f9ad87e2456a16b55e12e39456b2fa9a92
marsen <marsenzakirov11@gmail.com>
2022-06-23 18:02:06 +0300

    Add courses

--- pages/courses/courses.jsx
+++ pages/courses/courses.jsx
@@ -1,12 +1,81 @@
 import Header from "../../components/header/header";
 import SideBar from "../../components/sidebar/sidebar";
 import styles from "./courses.module.css";
+import { useState } from "react";
+import CardCourse from "../../components/cardCourse/cardCourse";
 export default function Courses() {
+  const [courses, setCourses] = useState([
+    {
+      title: "C++ 10light",
+      cost: 3020,
+      dscrp:
+        "На этом курсе вы ознокомитесь с основной базой с++. Этих знаний будет достаточно, чтобы решать задания ЕГЭ по информатике.",
+      fullDscrp:
+        "Lorem ipsum dolor sit amet. Et velit quas et dicta soluta aut error deleniti sit dolorem quibusdam tempore sapiente. Qui odio maxime et voluptatem facilis ut delectus architecto sed facilis consequatur At iure blanditiis id dolorum eius.Sed molestiae voluptas et laudantium nihil sit omnis magnam sed eligendi aut tenetur libero est excepturi quia. Eos asperiores enim ea mollitia quos hic impedit pariatur et consectetur dolorem aut quod sunt sed eius fuga. Et iure molestias a architecto iste ut dolore ratione et minus ducimus ab internos maxime qui porro dicta ut suscipit nemo. Aspernatur nostrum est distinctio quia non molestias velit sit internos magni et odio nihil et ipsam consequatur. ",
+    },
+    {
+      title: "C++ 10light",
+      cost: 3020,
+      dscrp:
+        "На этом курсе вы ознокомитесь с основной базой с++. Этих знаний будет достаточно, чтобы решать задания ЕГЭ по информатике.",
+      fullDscrp:
+        "Lorem ipsum dolor sit amet. Et velit quas et dicta soluta aut error deleniti sit dolorem quibusdam tempore sapiente. Qui odio maxime et voluptatem facilis ut delectus architecto sed facilis consequatur At iure blanditiis id dolorum eius.Sed molestiae voluptas et laudantium nihil sit omnis magnam sed eligendi aut tenetur libero est excepturi quia. Eos asperiores enim ea mollitia quos hic impedit pariatur et consectetur dolorem aut quod sunt sed eius fuga. Et iure molestias a architecto iste ut dolore ratione et minus ducimus ab internos maxime qui porro dicta ut suscipit nemo. Aspernatur nostrum est distinctio quia non molestias velit sit internos magni et odio nihil et ipsam consequatur. ",
+    },
+    {
+      title: "C++ 10light",
+      cost: 3020,
+      dscrp:
+        "На этом курсе вы ознокомитесь с основной базой с++. Этих знаний будет достаточно, чтобы решать задания ЕГЭ по информатике.",
+      fullDscrp:
+        "Lorem ipsum dolor sit amet. Et velit quas et dicta soluta aut error deleniti sit dolorem quibusdam tempore sapiente. Qui odio maxime et voluptatem facilis ut delectus architecto sed facilis consequatur At iure blanditiis id dolorum eius.Sed molestiae voluptas et laudantium nihil sit omnis magnam sed eligendi aut tenetur libero est excepturi quia. Eos asperiores enim ea mollitia quos hic impedit pariatur et consectetur dolorem aut quod sunt sed eius fuga. Et iure molestias a architecto iste ut dolore ratione et minus ducimus ab internos maxime qui porro dicta ut suscipit nemo. Aspernatur nostrum est distinctio quia non molestias velit sit internos magni et odio nihil et ipsam consequatur. ",
+    },
+    {
+      title: "C++ 10light",
+      cost: 3020,
+      dscrp:
+        "На этом курсе вы ознокомитесь с основной базой с++. Этих знаний будет достаточно, чтобы решать задания ЕГЭ по информатике.",
+      fullDscrp:
+        "Lorem ipsum dolor sit amet. Et velit quas et dicta soluta aut error deleniti sit dolorem quibusdam tempore sapiente. Qui odio maxime et voluptatem facilis ut delectus architecto sed facilis consequatur At iure blanditiis id dolorum eius.Sed molestiae voluptas et laudantium nihil sit omnis magnam sed eligendi aut tenetur libero est excepturi quia. Eos asperiores enim ea mollitia quos hic impedit pariatur et consectetur dolorem aut quod sunt sed eius fuga. Et iure molestias a architecto iste ut dolore ratione et minus ducimus ab internos maxime qui porro dicta ut suscipit nemo. Aspernatur nostrum est distinctio quia non molestias velit sit internos magni et odio nihil et ipsam consequatur. ",
+    },
+    {
+      title: "C++ 10light",
+      cost: 3020,
+      dscrp:
+        "На этом курсе вы ознокомитесь с основной базой с++. Этих знаний будет достаточно, чтобы решать задания ЕГЭ по информатике.",
+      fullDscrp:
+        "Lorem ipsum dolor sit amet. Et velit quas et dicta soluta aut error deleniti sit dolorem quibusdam tempore sapiente. Qui odio maxime et voluptatem facilis ut delectus architecto sed facilis consequatur At iure blanditiis id dolorum eius.Sed molestiae voluptas et laudantium nihil sit omnis magnam sed eligendi aut tenetur libero est excepturi quia. Eos asperiores enim ea mollitia quos hic impedit pariatur et consectetur dolorem aut quod sunt sed eius fuga. Et iure molestias a architecto iste ut dolore ratione et minus ducimus ab internos maxime qui porro dicta ut suscipit nemo. Aspernatur nostrum est distinctio quia non molestias velit sit internos magni et odio nihil et ipsam consequatur. ",
+    },
+    {
+      title: "C++ 12light",
+      cost: 3100,
+      dscrp:
+        "На этом курсе вы ознокомитесь с основной базой с++. Этих знаний будет достаточно, чтобы решать задания ЕГЭ по информатике.",
+      fullDscrp:
+        "Lorem ipsum dolor sit amet. Et velit quas et dicta soluta aut error deleniti sit dolorem quibusdam tempore sapiente. Qui odio maxime et voluptatem facilis ut delectus architecto sed facilis consequatur At iure blanditiis id dolorum eius.Sed molestiae voluptas et laudantium nihil sit omnis magnam sed eligendi aut tenetur libero est excepturi quia. Eos asperiores enim ea mollitia quos hic impedit pariatur et consectetur dolorem aut quod sunt sed eius fuga. Et iure molestias a architecto iste ut dolore ratione et minus ducimus ab internos maxime qui porro dicta ut suscipit nemo. Aspernatur nostrum est distinctio quia non molestias velit sit internos magni et odio nihil et ipsam consequatur. ",
+    },
+  ]);
   return (
     <>
       <Header />
       <SideBar />
-      <div className={styles.courses}>выыыыыыыыыыыыыыыыыыыыыыы</div>
+      <div className="container">
+        <div className={styles.courses}>
+          <h1 className={styles.title}>курсы</h1>
+          <ul className={styles.courses_list}>
+            {courses.map((card, index) => {
+              let [popupActive, setPopupActive] = useState(false);
+              return (
+                <CardCourse
+                  item={card}
+                  setPopupActive={setPopupActive}
+                  popupActive={popupActive}
+                  key={index}
+                />
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
